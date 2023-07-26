1f4dcd1eb1378bb845ce55982d21082a12b3a9e1
TongThanaphon <tongasura001@gmail.com>
2023-07-26 16:16:35 +0700

    u

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import Image from 'next/image'
 
+import sum from '@utils/sum'
+
 export default function Home() {
+  console.log(sum(3, 4))
+
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
